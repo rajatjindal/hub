a12d9da97ee3c07a1f82c36082e01343d0e9cbe7
Andrea Law <abylaw@gmail.com>
2018-06-21 22:26:45 -0400

    Style search page

--- src/js/views/SearchResults.vue
+++ src/js/views/SearchResults.vue
@@ -2,36 +2,39 @@
   <div class="index">
     <div class="columns">
       <div class="column is-one-fifth sidebar">
-        <topics-list v-model="topics"/>
+        <topics-list v-model="topics" v-if="topics.length > 0"/>
       </div>
       <div class="column is-8 main-container">
         <div class="search-bar-container">
           <search-bar :value="search" ref="searchBar"/>
         </div>
 
-        <div class="level is-mobile service-result-title-container">
-          <div class="level-left"><h2 class="is-marginless">{{totalItems}} service results</h2></div>
-          <div class="level-right">
-            <div class="select is-small">
-              <select v-model="orderBy">
-                <option selected value="NATURAL">Sort</option>
-                <option value="ALIAS_ASC">Name</option>
-              </select>
+        <div v-if="totalItems > 0">
+          <div class="level is-mobile service-result-title-container">
+            <div class="level-left"><h2 class="is-marginless">{{totalItems}} service results</h2></div>
+            <div class="level-right">
+              <div class="select is-small">
+                <select v-model="orderBy">
+                  <option selected value="NATURAL">Sort</option>
+                  <option value="ALIAS_ASC">Name</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div class="tile is-ancestor">
+              <div class="tile is-parent is-vertical">
+                <div v-for="r in results" class="tile is-child search-result">
+                  <service-summary :title="r.alias" :description="r.description" :tags="r.topics"></service-summary>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div>
-          <div class="tile is-ancestor">
-            <div class="tile is-parent is-vertical">
-              <div v-for="r in results" class="tile is-child search-result">
-                <service-summary :title="r.alias" :description="r.description" :tags="r.topics"></service-summary>
-              </div>
-              <div v-if="results.length === 0">
-                No search results found for "{{search}}".
-              </div>
-            </div>
-          </div>
+        <div v-else>
+          We couldn't find any services matching '{{search}}'
         </div>
       </div>
     </div>
@@ -89,6 +92,9 @@ h2
   line-height 1.8em
   margin-top 1em
 
+.index
+  min-height 650px
+
 .link
   cursor pointer
 
@@ -107,9 +113,10 @@ h2
 
 .search-bar-container
   margin-top 1.5em
-  margin-bottom 2em
+  margin-bottom 1em
 
 .service-result-title-container
+  margin-top 1em
   margin-bottom 0.8em
 
 .help-text
