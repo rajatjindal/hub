8aa107e6916f542c3d9da9a6182cf357a68ed9a4
Andrea Law <abylaw@gmail.com>
2018-06-29 14:18:07 -0400

    Add colors to service summary tags

--- src/js/components/ServiceSummary.vue
+++ src/js/components/ServiceSummary.vue
@@ -3,7 +3,7 @@
     <router-link :to="`/service/${title}`">
       <div class="media" v-if="!isLoading && title">
         <div class="media-left">
-          <div class="recently-added-image"></div>
+          <div class="service-image" :style="{ backgroundColor: color }">{{ firstLetterCapitalized }}</div>
         </div>
         <div class="media-content">
           <h4>{{ title }}</h4>
@@ -15,7 +15,7 @@
       </div>
       <div class="media" v-else-if="isLoading">
         <div class="media-left">
-          <div  class="loading-shimmer recently-added-image"></div>
+          <div  class="loading-shimmer service-image"></div>
         </div>
         <div class="media-content">
           <div class="loading-shimmer title"/>
@@ -28,9 +28,20 @@
 </template>
 
 <script scoped>
+import md5 from 'crypto-js/md5';
+
 export default {
   props: ['title', 'description', 'tags'],
   computed: {
+    color() {
+      const randomNumFromTitle = md5(this.title).words[0];
+      const numBetweenZeroAndOne = Math.abs(randomNumFromTitle) / 2147483647;
+      const hue = numBetweenZeroAndOne * 360;
+      return `hsla(${hue}, 45%, 65%, 1)`;
+    },
+    firstLetterCapitalized() {
+      return this.title && this.title.slice(0, 1).toUpperCase();
+    },
     isLoading() {
       return !this.title;
     },
@@ -45,11 +56,15 @@ export default {
 a
   color black
 
-.recently-added-image
+.service-image
   min-width: 65px;
   height: 65px;
   border-radius: 4px;
-  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.6em;
+  color: white;
 
 h4
   margin-top: 0;
@@ -70,7 +85,7 @@ p
 .loading-shimmer
   margin-bottom 0.8em
 
-  &.recently-added-image
+  &.service-image
     border-color transparent
 
   &.title
